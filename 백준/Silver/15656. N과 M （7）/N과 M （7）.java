e759115dc6cafc6b79859b2178304e4fabb61cef
이기태 <91439717+kitaee@users.noreply.github.com>
2024-10-30 11:11:24 +0900

    [Silver III] Title: N과 M (7), Time: 748 ms, Memory: 265736 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.time.*;
+import java.util.*;
+
+public class Main {
+
+    static int N,M;
+    static int[] arr;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] info = br.readLine().split(" ");
+        N = Integer.parseInt(info[0]);
+        M = Integer.parseInt(info[1]);
+
+        arr = new int[N];
+        result = new int[M];
+
+        info = br.readLine().split(" ");
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(info[i]);
+        }
+        Arrays.sort(arr);
+        dfs(0);
+        System.out.println(sb.toString());
+    }
+
+    static void dfs(int depth) {
+        if(depth == M) {
+            for(int i=0; i<M; i++) {
+                sb.append(result[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i=0; i<N; i++) {
+            result[depth] = arr[i];
+            dfs(depth+1);
+        }
+    }
+}
