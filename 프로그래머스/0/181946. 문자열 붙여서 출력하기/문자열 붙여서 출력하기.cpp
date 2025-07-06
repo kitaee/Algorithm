12ce0164fbe70c5f2eb3f98490033a3ea1bd930e
이기태 <91439717+kitaee@users.noreply.github.com>
2025-07-06 16:18:20 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 3.92 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str1, str2, answer;
+    cin >> str1 >> str2;
+    cout << str1+str2 << endl;
+    return 0;
+}
