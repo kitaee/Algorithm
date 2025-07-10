a7a04d40f98fdbff6c89cd841482fe6751c727af
이기태 <91439717+kitaee@users.noreply.github.com>
2025-07-10 15:17:48 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+
+    if(a==b && b==c) {
+        cout << 10000+(a*1000) << endl;
+    } else if(a==b || a==c) {
+        cout << 1000+(a*100) << endl;
+    } else if(b==c){
+        cout << 1000+(b*100) << endl;
+    } else {
+        cout << max(a, max(b,c))*100 << endl;
+    }
+}
